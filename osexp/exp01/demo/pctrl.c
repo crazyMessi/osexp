d9c2e3bc65268c9d3e2e58771abfd51b80fc6c44
ZHUODONG LI <1989218453@qq.com>
2021-04-14 16:56:52 +0800

    实验一

--- osexp/exp01/demo/pctrl.c
+++ osexp/exp01/demo/pctrl.c
@@ -1,62 +1,50 @@
-/*
-* Filename : pctrl.c
-* copyright : (C) 2006 by zhanghonglie
-* Function : 父子进程的并发执行
-*/
 #include "pctl.h"
 int main(int argc, char *argv[])
 {
-int i;
-int pid; //存放子进程号int status; //存放子进程返回状态
 char *args[] = {"/bin/ls","-a",NULL}; //子进程要缺省执行的命令
-signal(SIGINT,(sighandler_t)sigcat); //注册一个本进程处理键盘中断的函数
-pid=fork() ; //建立子进程
-if(pid<0) // 建立子进程失败?
+int pid_1,pid_2; //存放子进程号
+int status_1,status_2; //存放子进程返回状态
+while(1){
+pid_1=fork() ;
+if(pid_1<0) // 建立子进程失败?
 {
 printf("Create Process fail!\n");
 exit(EXIT_FAILURE);
 }
-if(pid == 0) // 子进程执行代码段
+if(pid_1 == 0) // 子进程执行代码段
 {
 //报告父子进程进程号
-printf("I am Child process %d\nMy father is %d\n",getpid(),getppid());
-pause(); //暂停，等待键盘中断信号唤醒
-//子进程被键盘中断信号唤醒继续执行
-printf("%d child will Running: ",getpid()); //
-if(argv[1] != NULL){
-//如果在命令行上输入了子进程要执行的命令
-//则执行输入的命令
-for(i=1; argv[i] != NULL; i++) printf("%s ",argv[i]);
-printf("\n");
-//装入并执行新的程序
-status = execve(argv[1],&argv[1],NULL);
-}
-else{
-//如果在命令行上没输入子进程要执行的命令
-//则执行缺省的命令
-for(i=0; args[i] != NULL; i++) printf("%s ",args[i]);
-printf("\n");
-//装入并执行新的程序
-status = execve(args[0],args,NULL);
-}
-}
-else //父进程执行代码段
+printf("I am Child-ls process %d\nMy father is %d\n",getpid(),getppid());/*getpid 返回当前进程
+的进程号，getppid 返回当前进程父进程的进程号*/
+pid_2=fork();
+if(pid_2<0)// 建立子进程失败?
 {
-printf("\nI am Parent process %d\n",getpid()); //报告父进程进程号
-if(argv[1] != NULL){
-//如果在命令行上输入了子进程要执行的命令
-//则父进程等待子进程执行结束
-printf("%d Waiting for child done.\n\n");
-waitpid(pid,&status,0); //等待子进程结束
-printf("\nMy child exit! status = %d\n\n",status);
+printf("Create Process fail!\n");
+exit(EXIT_FAILURE) ;
+}
+if(pid_2==0) // 子进程执行代码段
+{//报告父子进程进程号
+printf("I am Child-ps process %d\nMy father is %d\n",getpid(),getppid());
+printf("%d child will Running: \n",getpid()); /*子进程被键盘中断信号唤醒继续执行*/
+status_2=execve("/bin/ps",NULL,NULL);//装入并执行新的程序
+}else{
+printf("wait for the ps-child end%d\n",pid_2);
+waitpid(pid_2,&status_2,0);//等待子进程 2 结束
+//status 用于保留子进程的退出状态
+}
+printf("%d child will Running: \n",getpid()); //装入并执行新的程序 char *argv[]={"0",NULL};
+status_1 = execve("/bin/ls",argv,NULL);
 }
 else{
-//如果在命令行上没输入子进程要执行的命令
-//唤醒子进程，与子进程并发执行不等待子进程执行结束，
-if(kill(pid,SIGINT) >= 0)
-printf("%d Wakeup %d child.\n",getpid(),pid) ;
-printf("%d don't Wait for child done.\n\n",getpid());
+printf("I am Parent process %d\n",getpid());
+printf("wait for the ls-child end %d\n",pid_1);
+waitpid(pid_1,&status_1,0);
+printf("child end,sleep...\n");
+sleep(3);// sleep 函数会令调用进程的执行挂起睡眠 3 秒
 }
 }
 return EXIT_SUCCESS;
 }
+————————————————
+版权声明：本文为CSDN博主「Soul fragments」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/weixin_43943977/article/details/101833797
